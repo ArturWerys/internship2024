6fdadb91dc7d3eee8ebdc0dbd348533465e6a35c
Artur Werys <arturwerys@gmail.com>
2024-12-20 19:47:03 +0100

    Nowy sposob wyliczania wartosci promienia

--- GDS_shape_generator.py
+++ GDS_shape_generator.py
@@ -2,21 +2,11 @@ import gdspy
 from PIL import Image
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+import time
+import numpy as np
 
-# Python ma takie cos, co nazywa sie dictionary.
-# To jest taka array, ktora przechowuje haslo i jego wartosc w parach.
-# Przy definiowaniu funkcji to sie bardzo przydaje.
-# Po pierwsze, mozecie podawac argumenty w dowolnej kolejnosci.
-# Po drugie, mozecie je inicjalizowac z jakas wartoscia domyslna.
-# Po trzecie, tych, ktore maja wartosc domyslna, nie musicie za kazdym razem podawac.
-# To jest jak varargin w Matlabie, tylko znacznie lepsze.
 
-# przyklad:
-# def draw_circle(library,cell_name,radius=1,size_of_cell=2,layer_numer=1,x_px=0,y_px=0):
-# i teraz mozecie wywolac funkcje np. tak:
-    #def draw_circle(library,cell_name,radius=1,size_of_cell=2,layer_number=1,x_px=0,y_px=0):
-# draw_circle(lib,main_cell,x_px=i,y_px=j)
-def draw_circle(library, cell_name, radius, size_of_cell, layer_number, x_px, y_px):
+def draw_circle(library, cell_name, radius=1, size_of_cell=2, layer_number=1, x_px=0, y_px=0):
     center_x = x_px * size_of_cell
     center_y = - y_px * size_of_cell
     circle = gdspy.Round((center_x, center_y), radius, layer=layer_number)
@@ -29,43 +19,54 @@ Tk().withdraw()
 filepath = askopenfilename(title="Wybierz plik obrazu", filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif")])
 
 if filepath:
+
     image = Image.open(filepath)
     lib = gdspy.GdsLibrary()
-    main_cell = lib.new_cell('Main') # zeby mi to dzialalo, za kazdym razem musze zmieniac te nazwe, bo inaczej jest error
+    cell_name = lib.new_cell(f"Cell_{int(time.time())}")  # Dodanie znacznika czasu
 
     print(f"Obraz wczytany: {filepath}")
+
     x, y = image.size
 
-# gdzies trzeba zdefiniowac:
-# jaki wymiar ma piksel (czyli o ile skaczemy w petli)
-# jaka jest srednica albo promien kolka dla minimalnej wartosci piksela
-# jaka jest srednica albo promien kolka dla maksymalnej wartosci piksela
-# pamietajcie, ze wartosc piksela ma byc proporcjonalna do powierzchni kola, a nie samego promienia (zeleznosc niekoniecznie bedzie liniowa)
+    pixels = []
+    radii = []
 
     for i in range(x):
         for j in range(y):
+
             pixel = image.getpixel((i, j))
+            pixels.append(pixel)
+
             print(f"Wartość piksela: {pixel}")
 
-            layer = 11
+            r_max = int(np.sqrt(max(pixels) / np.pi))
+            r_min = int(np.sqrt(min(pixels) / np.pi))
 
-            for ll in range(layer + 1):
-                if ll <= 10:
-                    if 0 + 25 * ll < pixel <= 25 + 25 * ll:
-                        draw_circle(lib, main_cell, 1, 2, ll, i, j) # tu juz macie wszystko co trzeba, musicie tylko modyfikowac powierzchnie kolka zamiast warstwy
+            if r_min < 1:
+                r_min = 1
 
-                # if l == 11 and not executed_once_for_11:
-                #     draw_circle(lib, main_cell, 1, 2, 11, i, j)
-                #     executed_once_for_11 = True
+            radii.append(r_min)
+            radii.append(r_max)
 
-            # if 0 < pixel <= 25:
-            #     draw_circle(lib, main_cell, 1, 2, 1, i, j)
-            # elif 63 < pixel <= 127:
-            #     draw_circle(lib, main_cell, 1, 2, 2, i, j)
-            # elif 127 < pixel <= 191:
-            #     draw_circle(lib, main_cell, 1, 2, 3, i, j)
-            # elif 191 < pixel < 255:
-            #     draw_circle(lib, main_cell, 1, 2, 4, i, j)
+            radius_range = r_max - r_min
+
+            print(r_min)
+            print(r_max)
+
+            size_of_cell = r_max
+
+            area_min = np.pi * (r_min ** 2)
+            area_max = np.pi * (r_max ** 2)
+
+            if pixel > 0:
+                area = area_min + (pixel / 255) * (area_max - area_min)
+                r_new = np.sqrt(area / np.pi)
+                radii.append(r_new)
+            else:
+                r_new = 0
+
+            if r_new > 0:
+                draw_circle(lib, cell_name, r_new, size_of_cell, layer_number=1, x_px=i, y_px=j)
 
 else:
     print("Nie wybrano pliku.")
