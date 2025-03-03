a62001b57f2bb6323c4cad23469eb91b0de8dee2
Artur Werys <arturwerys@gmail.com>
2025-03-03 23:11:37 +0100

    Generacja powierzchni GDS

--- Ebeam_gds_files.py
+++ Ebeam_gds_files.py
@@ -3,19 +3,19 @@ import time
 from Diffraction_Grating import generate_gds_file as gds
 
 # Pierwsza soczewka
-pimg("C:\Programowanie\MatLab\Cezamat\lens1.png", "pow_1.gds", 1600, 400, 800, 2, [])
+pimg("filepath", "pow_1.gds", 1600, 400, 800, 2, [])
 time.sleep(1)
 # r min > 200
 
 # Druga soczewka
-pimg("C:\Programowanie\MatLab\Cezamat\lens1_neg.png", "pow_1_neg.gds", 1600, 400, 800, 3, [])
+pimg("filepath", "pow_1_neg.gds", 1600, 400, 800, 3, [])
 time.sleep(1)
 
 # Generacja siatek dyfrakcyjnych
-gds("Dif_Grat_3_8.gds",1,25,3,1,8)
+gds("Dif_Grat_3_8.gds", 1, 25, 3, 1, 8)
 time.sleep(1)
 
-gds("Dif_Grat_2_7.gds",1,30,2,1,7)
+gds("Dif_Grat_2_7.gds", 1, 30, 2, 1, 7)
 time.sleep(1)
 
-gds("Dif_Grat_0_7_20.gds",1,70,0.7,1,20)
+gds("Dif_Grat_0_7_20.gds", 1, 70, 0.7, 1, 20)
